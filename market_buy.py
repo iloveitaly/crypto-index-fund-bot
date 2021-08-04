a2939308041af997adc57bb0fdcdd170472e03ba
Michael Bianco <iloveitaly@gmail.com>
2021-08-04 07:19:14 -0600

    Add reserve amount and fixed misc bugs

--- market_buy.py
+++ market_buy.py
@@ -1,16 +1,13 @@
 from utils import log
-import math
 from user import User
 
-from portfolio import add_price_to_portfolio, portfolio_with_allocation_percentages, add_price_to_portfolio, merge_portfolio
-from exchanges import binance_portfolio, can_buy_amount_in_exchange, price_of_symbol
-from market_cap import coins_with_market_cap
-from convert_stablecoins import convert_stablecoins
+import math
+from exchanges import binance_normalize_purchase_amount, binance_purchase_minimum, can_buy_amount_in_exchange, price_of_symbol
 
 from data_types import CryptoBalance, CryptoData, MarketBuy
-from typing import List
+import typing as t
 
-def calculate_market_buy_preferences(target_index: List[CryptoData], current_portfolio: List[CryptoBalance]) -> List[CryptoData]:
+def calculate_market_buy_preferences(target_index: t.List[CryptoData], current_portfolio: t.List[CryptoBalance]) -> t.List[CryptoData]:
   """
   Buying priority:
 
@@ -60,21 +57,28 @@ def calculate_market_buy_preferences(target_index: List[CryptoData], current_por
 
   return index_by_unowned_assets
 
-def purchasing_currency_in_portfolio(user: User, portfolio: List[CryptoBalance]) -> float:
-  return math.fsum([
+def purchasing_currency_in_portfolio(user: User, portfolio: t.List[CryptoBalance]) -> float:
+  # ideally, we wouldn't need to have a reserve amount. However, FP math is challenging and it's easy
+  # to be off a cent or two. It's easier just to reserve $1 and not deal with it. Especially for a fun project.
+  reserve_amount = 1
+
+  # TODO we should use Decimal here instead of float
+  total = math.fsum([
     balance['usd_total']
     for balance in portfolio
     if balance['symbol'] == user.purchasing_currency()
   ])
 
+  return max(total - reserve_amount, 0)
+
 # TODO pass exchange reference into this method and remove hardcoded binance stuff
 def determine_market_buys(
     user: User,
-    sorted_buy_preferences: List[CryptoData],
-    current_portfolio: List[CryptoBalance],
-    target_portfolio: List[CryptoData],
+    sorted_buy_preferences: t.List[CryptoData],
+    current_portfolio: t.List[CryptoBalance],
+    target_portfolio: t.List[CryptoData],
     purchase_balance: float,
-  ) -> List[MarketBuy]:
+  ) -> t.List[MarketBuy]:
   """
   1. Purchase minimums for each asset
   3. Amount of purchasing currency available
@@ -86,13 +90,13 @@ def determine_market_buys(
 
   # it doesn't look like this is specified in the API, and the minimum is different
   # depending on if you are using the pro vs simple view
-  binance_purchase_minimum = 10
+  exchange_purchase_minimum = binance_purchase_minimum()
 
   purchase_minimum = user.purchase_min()
   purchase_maximum = user.purchase_max()
   portfolio_total = math.fsum(balance['usd_total'] for balance in current_portfolio)
 
-  if purchase_balance < binance_purchase_minimum:
+  if purchase_balance < exchange_purchase_minimum:
     log.info("not enough USD to buy anything", purchase_balance=purchase_balance)
     return []
 
@@ -105,7 +109,7 @@ def determine_market_buys(
     target_info = next((target for target in target_portfolio if target['symbol'] == coin['symbol']))
 
     # TODO consider extracting price calculation logic into a separate method
-    purchase_amount = purchase_total if purchase_total < binance_purchase_minimum * 2 else purchase_minimum
+    purchase_amount = purchase_total if purchase_total < exchange_purchase_minimum * 2 else purchase_minimum
 
     # percentage is not expressed in a < 1 float, so we need to convert it
     target_amount = target_info['percentage'] / 100.0 * portfolio_total
@@ -114,7 +118,7 @@ def determine_market_buys(
     purchase_amount = min(purchase_amount, target_amount, purchase_maximum)
 
     # we need to at least buy the minimum that the exchange allows
-    purchase_amount = max(binance_purchase_minimum, purchase_amount)
+    purchase_amount = max(exchange_purchase_minimum, purchase_amount)
 
     if purchase_amount > purchase_total:
       log.info("not enough purchase currency balance for coin", balance=purchase_total, coin=coin['symbol'])
@@ -142,7 +146,7 @@ def determine_market_buys(
   return purchases
 
 # https://www.binance.us/en/usercenter/wallet/money-log
-def make_market_buys(user: User, market_buys: List[MarketBuy]) -> List:
+def make_market_buys(user: User, market_buys: t.List[MarketBuy]) -> t.List:
   purchasing_currency = user.purchasing_currency()
   binance_client = user.binance_client()
   orders = []
@@ -155,9 +159,7 @@ def make_market_buys(user: User, market_buys: List[MarketBuy]) -> List:
 
     # symbol is: `baseasset` + `quoteasset`
     purchase_symbol = buy['symbol'] + purchasing_currency
-    symbol_info = binance_client.get_symbol_info(purchase_symbol)
-    # TODO maybe use Decimal.quantize() to round?
-    normalized_amount = round(buy['amount'], symbol_info['quoteAssetPrecision'])
+    normalized_amount = binance_normalize_purchase_amount(buy['amount'], purchase_symbol)
 
     order_params = {
       'symbol': purchase_symbol,
@@ -182,6 +184,7 @@ def make_market_buys(user: User, market_buys: List[MarketBuy]) -> List:
       log.info(
         "price analytics",
 
+        symbol=purchase_symbol,
         difference=str(ask_difference),
         percentage_difference=str(ask_difference / Decimal(lowest_ask) * Decimal(-100.0)),
         bid=highest_bid,
@@ -189,24 +192,19 @@ def make_market_buys(user: User, market_buys: List[MarketBuy]) -> List:
         reported_price=price_of_symbol(buy['symbol'], purchasing_currency)
       )
 
-      # TODO should consider using `Decimal` below instead of `float`
-
       # TODO can we inspect the order book depth here? Or general liquidity for the market?
       #      what else can we do to improve our purchase strategy?
-
-      # the quote precision is not what we need to round by, the stepSize needs to be used instead:
-      # https://github.com/sammchardy/python-binance/issues/219
-      step_size = next(f['stepSize'] for f in symbol_info['filters'] if f['filterType'] == 'LOT_SIZE')
-      precision_from_step_size = int(round(-math.log(float(step_size), 10), 0))
 
       # TODO add option to use the midpoint, or some other position, of the order book instead of the lowest ask
 
       order_params |= {
         # TODO is there a way to specify a number of hours? It seems like only the three standard TIF options are available
         'timeInForce': 'GTC',
-        'quantity': round(normalized_amount / float(highest_bid), precision_from_step_size),
+        'quantity': binance_normalize_purchase_amount(Decimal(normalized_amount) / Decimal(highest_bid), purchase_symbol),
         'price': highest_bid
       }
+
+      log.info("submitting limit buy order", order=order_params)
     else: # market
       order_params |= {
         # `quoteOrderQty` allows us to purchase crypto in a currency of choice, instead of an amount in the token we are buying
@@ -214,24 +212,46 @@ def make_market_buys(user: User, market_buys: List[MarketBuy]) -> List:
         'quoteOrderQty': normalized_amount,
       }
 
-    log.info("submitting market buy order", order=order_params)
+      log.info("submitting market buy order", order=order_params)
 
     from binance.exceptions import BinanceAPIException
 
-    if user.livemode:
-      if user.buy_strategy == 'limit':
-        order = binance_client.order_limit_buy(**order_params)
+    """
+    Order structure:
+
+    [{'clientOrderId': 'Egjlu8owfhb0GTnp5auohS',
+    'cummulativeQuoteQty': '0.0000',
+    'executedQty': '0.00000000',
+    'fills': [],
+    'orderId': 18367859,
+    'orderListId': -1,
+    'origQty': '0.24700000',
+    'price': '56.8830',
+    'side': 'BUY',
+    'status': 'NEW',
+    'symbol': 'ZENUSD',
+    'timeInForce': 'GTC',
+    'transactTime': 1628012040277,
+    'type': 'LIMIT'}]
+    """
+
+    try:
+      if user.livemode:
+        if user.buy_strategy == 'limit':
+          order = binance_client.order_limit_buy(**order_params)
+        else:
+          order = binance_client.order_market_buy(**order_params)
       else:
-        order = binance_client.order_market_buy(**order_params)
-    else:
-      from binance.client import Client
+        from binance.client import Client
 
-      # if test is successful, order will be an empty dict
-      order = binance_client.create_test_order(**({
-        'side': Client.SIDE_BUY,
-        'type': Client.ORDER_TYPE_LIMIT if user.buy_strategy == 'limit' else Client.ORDER_TYPE_MARKET,
-      } | order_params))
+        # if test is successful, order will be an empty dict
+        order = binance_client.create_test_order(**({
+          'side': Client.SIDE_BUY,
+          'type': Client.ORDER_TYPE_LIMIT if user.buy_strategy == 'limit' else Client.ORDER_TYPE_MARKET,
+        } | order_params))
 
-    orders.append(order)
+      orders.append(order)
+    except BinanceAPIException as e:
+      log.error("failed to submit market buy order", error=e)
 
   return orders
