37d6c9fc1d5f95897c8fbf76f331b9a8229a5016
Michael Bianco <iloveitaly@gmail.com>
2021-08-11 05:59:10 -0600

    Adding util to cancel open orders

---
+++ open_orders.py
@@ -0,0 +1,34 @@
+import datetime
+import typing as t
+
+from user import User, user_from_env
+from utils import log
+from binance.client import Client as BINANCE_CONSTANTS
+
+# TODO abstract out binance specifics
+def cancel_stale_open_orders(user: User) -> t.List:
+  binance_client = user.binance_client()
+  order_time_limit = 24
+
+  old_orders = [
+    order
+    for order in binance_client.get_open_orders()
+    if order['side'] == BINANCE_CONSTANTS.SIDE_BUY and order['timeInForce'] == BINANCE_CONSTANTS.TIME_IN_FORCE_GTC and
+    # `time` from binance is expressed in milliseconds
+    order['time'] < (datetime.datetime.now() - datetime.timedelta(hours=order_time_limit)).timestamp() * 1000.0
+  ]
+
+  if not old_orders:
+    log.info('no stale open orders')
+    return []
+
+  cancelled_orders = []
+
+  for order in old_orders:
+    log.info('cancelling order', order=order)
+    cancelled_orders.append(binance_client.cancel_order(symbol=order['symbol'], orderId=order['orderId']))
+
+  return cancelled_orders
+
+if __name__ == '__main__':
+  cancel_stale_open_orders(user_from_env())
