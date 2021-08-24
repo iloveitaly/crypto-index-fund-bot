bf698bdd8e607e98ffea951886b409ac64d71f15
Michael Bianco <iloveitaly@gmail.com>
2021-08-24 12:39:39 -0600

    Adding django scaffold

---
+++ botweb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for botweb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'botweb.settings')
+
+application = get_asgi_application()
