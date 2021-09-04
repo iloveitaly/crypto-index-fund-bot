ccd71e5ce2a092371fd0b1ad9fbe0a2568d19235
Michael Bianco <iloveitaly@gmail.com>
2021-09-04 16:24:34 -0600

    Cleanup multi user test

--- test/test_multi_user.py
+++ test/test_multi_user.py
@@ -6,22 +6,10 @@ from users.models import User
 import users.tasks
 import bot.commands
 
-# @pytest.fixture
-# def celery_enable_logging():
-#     return True
-
-@pytest.fixture
-# # @pytest.fixture(scope='session')
-def celery_config():
-    return {
-        'broker_url': 'memory://',
-        # 'result_backend': 'redis://'
-    }
+# Specifying `@pytest.mark.usefixtures('celery_session_worker')` causes issues with database cleaning
 
 @pytest.mark.django_db
 class TestMultiUser(unittest.TestCase):
-  # @pytest.mark.celery(CELERY_ALWAYS_EAGER=True)
-  # @patch('bot.commands.BuyCommand.execute')
   @patch.object(bot.commands.BuyCommand, 'execute')
   def test_performs_market_buy(self, buy_command_mock):
     user_1 = User.objects.create(name="user 1")
@@ -29,6 +17,6 @@ class TestMultiUser(unittest.TestCase):
 
     # users.tasks.initiate_user_buys.delay()
     # breakpoint()
-    users.tasks.initiate_user_buys()
+    users.tasks.initiate_user_buys.delay()
 
     assert buy_command_mock.call_count == 2
