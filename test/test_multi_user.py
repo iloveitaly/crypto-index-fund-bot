eddf9b2cf2bacc6362c088fcd2ddb13ddfcce00f
Michael Bianco <iloveitaly@gmail.com>
2021-09-04 20:03:56 -0600

    Some test cleanup

--- test/test_multi_user.py
+++ test/test_multi_user.py
@@ -13,10 +13,8 @@ class TestMultiUser(unittest.TestCase):
   @patch.object(bot.commands.BuyCommand, 'execute')
   def test_performs_market_buy(self, buy_command_mock):
     user_1 = User.objects.create(name="user 1")
-    user_1 = User.objects.create(name="user 2")
+    user_2 = User.objects.create(name="user 2")
 
-    # users.tasks.initiate_user_buys.delay()
-    # breakpoint()
     users.tasks.initiate_user_buys.delay()
 
     assert buy_command_mock.call_count == 2
