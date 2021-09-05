f02f154ec08d0712de55815fc5368f6803f6426e
Michael Bianco <iloveitaly@gmail.com>
2021-09-04 18:49:21 -0600

    Conditionally load sentry in prod

--- botweb/settings/production.py
+++ botweb/settings/production.py
@@ -1,1 +1,10 @@
 from .application import *
+
+from decouple import config
+
+if sentry_dsn := config("SENTRY_DSN", default=False)
+  import sentry_sdk
+  sentry_sdk.init(
+    sentry_dsn,
+    traces_sample_rate=1.0,
+  )
