48d5b000dec2390803dee270e43da71a32320243
Michael Bianco <iloveitaly@gmail.com>
2021-09-04 18:47:23 -0600

    More tests for order prioritization

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,8 +1,8 @@
 import unittest
-from bot.user import user_from_env
 import pytest
-
 from unittest.mock import patch
+
+from bot.user import user_from_env
 from click.testing import CliRunner
 
 import binance.client
@@ -34,6 +34,72 @@ def vcr_config():
 
 @pytest.mark.vcr
 class TestE2E(unittest.TestCase):
+  PURCHASE_MIN = 25
+
+  # initial buys should prioritize coins that take up a large amount of the index first
+  @patch.object(binance.client.Client, 'order_market_buy', return_value={})
+  @patch('bot.exchanges.binance_portfolio', return_value=[])
+  def test_initial_buy(self, _binance_portfolio_mock, order_market_buy_mock):
+    from bot.commands import BuyCommand
+
+    user = user_from_env()
+    user.external_portfolio = []
+    assert user.external_portfolio == []
+    assert set(user.deprioritized_coins) == set(['DOGE', 'XRP', 'BNB'])
+    assert True == user.livemode
+    assert self.PURCHASE_MIN == user.purchase_min
+    assert MarketBuyStrategy.MARKET == user.buy_strategy
+    assert MarketIndexStrategy.MARKET_CAP == user.index_strategy
+
+    BuyCommand.execute(user=user, purchase_balance=self.PURCHASE_MIN * 3)
+
+    # make sure the user minimum is respected
+    assert float(order_market_buy_mock.mock_calls[0].kwargs['quoteOrderQty']) == self.PURCHASE_MIN
+
+    all_order_tokens = [
+      mock_call.kwargs['symbol']
+      for mock_call in order_market_buy_mock.mock_calls
+    ]
+
+    # top market tokens should be prioritized
+    assert set(all_order_tokens) == set(['BTCUSD', 'ETHUSD', 'ADAUSD'])
+
+  @patch.object(binance.client.Client, 'order_market_buy', return_value={})
+  @patch('bot.exchanges.binance_portfolio', return_value=[])
+  def test_off_allocation_portfolio(self, _binance_portfolio_mock, order_market_buy_mock):
+    from bot.commands import BuyCommand
+
+    user = user_from_env()
+    user.external_portfolio = [
+      {
+        "symbol": "DOGE",
+        "amount": 1_000_000
+      },
+      {
+        "symbol": "ETH",
+        "amount": 0.05
+      },
+      {
+        "symbol": "BTC",
+        "amount": 0.05
+      }
+    ]
+    assert set(user.deprioritized_coins) == set(['DOGE', 'XRP', 'BNB'])
+    assert True == user.livemode
+    assert self.PURCHASE_MIN == user.purchase_min
+    assert MarketBuyStrategy.MARKET == user.buy_strategy
+    assert MarketIndexStrategy.MARKET_CAP == user.index_strategy
+
+    BuyCommand.execute(user=user, purchase_balance=self.PURCHASE_MIN * 4)
+
+    all_order_tokens = [
+      mock_call.kwargs['symbol']
+      for mock_call in order_market_buy_mock.mock_calls
+    ]
+
+    # top market tokens should be prioritized
+    assert set(all_order_tokens) == set(['BTCUSD', 'ETHUSD', 'ADAUSD', 'SOLUSD'])
+
   def test_not_buying_open_orders(self):
     pass
 
@@ -53,7 +119,7 @@ class TestE2E(unittest.TestCase):
     # output is redirected to the result object, let's output for debugging
     print(result.output)
 
-    # assert result.exception is None
+    assert result.exception is None
     assert result.exit_code == 0
 
     # 60 should be split into two orders
