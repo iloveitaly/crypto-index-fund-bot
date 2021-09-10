b157f2057ecb88ae9d61b253271658850f2c315d
Michael Bianco <iloveitaly@gmail.com>
2021-09-10 05:33:32 -0600

    Test for binance data equivilence

---
+++ test/test_binance.py
@@ -0,0 +1,14 @@
+import pytest
+import unittest
+
+import bot.exchanges as exchanges
+
+@pytest.mark.vcr
+class TestBinance(unittest.TestCase):
+    # this invariant is important to the bot making correct decisions
+    def test_individual_and_batch_symbols(self):
+        for target_trading_pair in ['BTCUSD', 'ETHUSD']:
+            symbol_info_from_batch = exchanges.binance_get_symbol_info(target_trading_pair)
+            symbol_info_directly = exchanges.public_binance_client().get_symbol_info(target_trading_pair)
+
+            assert symbol_info_from_batch == symbol_info_directly
