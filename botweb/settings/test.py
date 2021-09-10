47b634fff2f9f659425ca68f67d2eb2de611c4c4
Michael Bianco <iloveitaly@gmail.com>
2021-09-10 06:41:58 -0600

    Adding default secret key for ci

--- botweb/settings/test.py
+++ botweb/settings/test.py
@@ -10,3 +10,4 @@ CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
 
 DATABASES = {"default": dj_database_url.parse(config("TEST_DATABASE_URL"))}
+SECRET_KEY = config("DJANGO_SECRET_KEY", defaul="django-insecure-@o-)qrym-cn6_*mx8dnmy#m4*$j%8wyy+l=)va&pe)9e7@o4i)")
