fffa76162f47879fb7a261576819468dc4d1fc93
Michael Bianco <iloveitaly@gmail.com>
2021-09-14 06:53:21 -0600

    Autoload user from env

--- botweb/settings/development.py
+++ botweb/settings/development.py
@@ -3,9 +3,7 @@ from .application import *
 DATABASES = {"default": dj_database_url.parse(config("TEST_DATABASE_URL"))}
 
 # https://stackoverflow.com/questions/19236771/add-method-imports-to-shell-plus
-SHELL_PLUS_PRE_IMPORTS = [
-    ("decimal", "Decimal"),
-]
+SHELL_PLUS_PRE_IMPORTS = [("decimal", "Decimal"), ("bot.user", "user_from_env")]
 
 # TODO we should log SQL by default
 # https://avilpage.com/2018/05/django-tips-tricks-log-sql-queries-to-console.html
