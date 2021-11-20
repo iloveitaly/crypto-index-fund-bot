b7e447921967046fc815304f620f0a051b3d8f02
Michael Bianco <iloveitaly@gmail.com>
2021-11-20 11:13:04 -0700

    Adding migration to add two new fields

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -3,25 +3,26 @@
 import encrypted_model_fields.fields
 from django.db import migrations, models
 
+import users.models
+
 
 class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('binance_api_key', encrypted_model_fields.fields.EncryptedCharField(null=True)),
-                ('binance_secret_key', encrypted_model_fields.fields.EncryptedCharField(null=True)),
-                ('external_portfolio', models.JSONField(default=dict)),
-                ('preferences', models.JSONField(default=dict)),
-                ('name', models.CharField(max_length=100)),
-                ('date_checked', models.DateTimeField(null=True)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("binance_api_key", encrypted_model_fields.fields.EncryptedCharField(null=True)),
+                ("binance_secret_key", encrypted_model_fields.fields.EncryptedCharField(null=True)),
+                ("external_portfolio", models.JSONField(decoder=users.models.CustomJSONDecoder, default=dict)),
+                ("preferences", models.JSONField(default=dict)),
+                ("name", models.CharField(max_length=100)),
+                ("date_checked", models.DateTimeField(null=True)),
             ],
         ),
     ]
