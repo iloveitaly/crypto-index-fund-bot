436965f1194c8fa5032cb41bfa395bfdbe176965
Michael Bianco <iloveitaly@gmail.com>
2021-12-06 07:02:48 -0700

    Auto import portfolio command in shell_plus

--- botweb/settings/development.py
+++ botweb/settings/development.py
@@ -3,7 +3,7 @@ from .application import *
 DATABASES = {"default": dj_database_url.parse(config("TEST_DATABASE_URL"))}
 
 # https://stackoverflow.com/questions/19236771/add-method-imports-to-shell-plus
-SHELL_PLUS_PRE_IMPORTS = [("decimal", "Decimal"), ("bot.user", "user_from_env")]
+SHELL_PLUS_PRE_IMPORTS = [("decimal", "Decimal"), ("bot.user", "user_from_env"), ("bot.commands", "PortfolioCommand")]
 
 # TODO we should log SQL by default
 # https://avilpage.com/2018/05/django-tips-tricks-log-sql-queries-to-console.html
